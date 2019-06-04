c46d3bf4c24e142d0ba75ac4692feb0ead0cf7d8
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-06-04 15:05:44 +0300

    Migrated part of NuciXNA.DataAccess here

---
+++ DataObjects/EntityBase.cs
@@ -0,0 +1,13 @@
+namespace NuciDAL.DataObjects
+{
+    public class EntityBase : EntityBase<string> { }
+
+    public class EntityBase<TKey>
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public TKey Id { get; set; }
+    }
+}
