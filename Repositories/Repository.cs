8ba5a27633793d3c90648f7a31d0c90bcefed174
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-06 10:24:11 +0200

    exception-handling: Improved EntityNotFound throwing

--- Repositories/Repository.cs
+++ Repositories/Repository.cs
@@ -39,7 +39,7 @@ namespace NuciDAL.Repositories
         /// <param name="entity">Entity.</param>
         public virtual void Add(TDataObject entity)
         {
-            if (Entities.ContainsKey(entity.Id))
+            if (ContainsId(entity.Id))
             {
                 throw new EntityAlreadyExistsException(
                     entity.Id.ToString(),
@@ -67,7 +67,8 @@ namespace NuciDAL.Repositories
         /// </summary>
         /// <returns>A boolean representing whether an entity with the specified identifier exists.</returns>
         /// <param name="id">Identifier.</param>
-        public virtual bool ContainsId(TKey id) => Entities.ContainsKey(id);
+        public virtual bool ContainsId(TKey id)
+            => Entities.ContainsKey(id);
 
         /// <summary>
         /// Get the entity with the specified identifier.
@@ -76,12 +77,9 @@ namespace NuciDAL.Repositories
         /// <param name="id">Identifier.</param>
         public virtual TDataObject Get(TKey id)
         {
-            if (!Entities.TryGetValue(id, out TDataObject value))
-            {
-                throw new EntityNotFoundException(id.ToString(), nameof(TDataObject));
-            }
+            EnsureEntityExists(id);
 
-            return value;
+            return Entities[id];
         }
 
         /// <summary>
@@ -89,7 +87,7 @@ namespace NuciDAL.Repositories
         /// </summary>
         /// <returns>A random entity.</returns>
         public virtual TDataObject GetRandom()
-            => Entities.Values.GetRandomElement();
+            => GetAll().GetRandomElement();
 
         /// <summary>
         /// Tries to get the entity with the specified identifier.
@@ -102,7 +100,7 @@ namespace NuciDAL.Repositories
             {
                 return Get(id);
             }
-            catch (Exception)
+            catch
             {
                 return null;
             }
@@ -112,7 +110,8 @@ namespace NuciDAL.Repositories
         /// Gets all the entities.
         /// </summary>
         /// <returns>The entities</returns>
-        public virtual IEnumerable<TDataObject> GetAll() => Entities.Values;
+        public virtual IEnumerable<TDataObject> GetAll()
+            => Entities.Values;
 
         /// <summary>
         /// Updates the specified entity's fields.
@@ -148,7 +147,12 @@ namespace NuciDAL.Repositories
         /// Removes the specified entity.
         /// </summary>
         /// <param name="entity">Entity.</param>
-        public virtual void Remove(TDataObject entity) => Entities.Remove(entity.Id);
+        public virtual void Remove(TDataObject entity)
+        {
+            EnsureEntityExists(entity.Id);
+
+            Entities.Remove(entity.Id);
+        }
 
         /// <summary>
         /// Tries to remove the specified entity.
@@ -180,6 +184,16 @@ namespace NuciDAL.Repositories
                 Remove(id);
             }
             catch { }
+        }
+
+        void EnsureEntityExists(TKey id)
+        {
+            if (!ContainsId(id))
+            {
+                throw new EntityNotFoundException(
+                    id.ToString(),
+                    typeof(TDataObject));
+            }
         }
     }
 }
